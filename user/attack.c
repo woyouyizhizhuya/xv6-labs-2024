6d76538ffe2b209792813fee4175f26615bf7f7b
woyouyizhizhuya <714741274@qq.com>
2025-01-10 17:05:24 +0800

    attacktest

--- user/attack.c
+++ user/attack.c
@@ -8,6 +8,12 @@ main(int argc, char *argv[])
 {
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
-
+  if (argc != 1){
+    fprintf(2, "Usage: attack\n");
+  }
+  char *end = sbrk(PGSIZE*32);
+  end = end + 16 * PGSIZE;
+  printf("secret: %s\n", end + 32);
+  write(2, end + 32, 8);
   exit(1);
 }
